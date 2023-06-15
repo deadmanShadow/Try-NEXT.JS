50bdcc447e1aab32e61bf30054dc6e09f2c08dfe
anUnnecessary <raihanshamil33@gmail.com>
2023-06-15 11:43:34 +0600

    third commit with adding year

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,6 +3,9 @@ import React from 'react';
 const DashboardLayout = ( { children }) => {
     return (
         <div>
+            <div>
+                Sidebar
+            </div>
             {children}
         </div>
     );
